6fd07d292d0171d91b91a732cceaaa8b45c6b32e
Alexandre Tisserant <alexandre.tisserant@ntnu.no>
2016-02-19 10:50:24 +0100

    corrected formula of the volume of sphere and added a readme

---
+++ README.rst
@@ -0,0 +1,4 @@
+INTRO
+=====
+
+git and pycharm tests
--- test2.py
+++ test2.py
@@ -1,9 +1,11 @@
+import numpy as np
+
 def poweration(a,b):
     return a**b
 
 def SphereVolume(a, type='radius'):
     if type == 'radius':
-        a = a
+        pass
     elif type == 'diameter':
-        a = a/2
-    return (4/3)*poweration(a,3)
+        a /= 2
+    return (4/3)*np.pi*poweration(a,3)
